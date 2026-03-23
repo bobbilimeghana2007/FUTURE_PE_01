322e78e7681336651c772084fcdd95557b71055a
Meghana Bobbili <bobbilimeghana2007@gmail.com>
2026-03-23 22:08:16 +0530

    Add files via upload

---
+++ Task1_Cafe_Website/script.js
@@ -0,0 +1,8 @@
+function bookTable(event){
+
+event.preventDefault();
+
+document.getElementById("message").innerText =
+"✅ Your table reservation request has been received! We will contact you soon.";
+
+}
